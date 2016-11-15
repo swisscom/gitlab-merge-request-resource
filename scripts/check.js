2e524ecfd13c9ce0dda07b00b165743d5e842d9a
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-11-15 11:28:38 +0100

    Make check response valid JSON

--- scripts/check.js
+++ scripts/check.js
@@ -5,5 +5,5 @@ const check = require('./lib/check');
 const payload = JSON.parse(process.argv[2]);
 
 check(payload)
-  .then((versions) => console.log(versions))
+  .then((versions) => console.log(JSON.stringify(versions)))
   .catch((err) => console.error(err));
